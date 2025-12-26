f657402a4bd3d001c9f6806ca6a8a2f21a05f4bb
keshavmodani07 <keshavmodani07@gmail.com>
2025-12-26 18:53:06 +0530

    double pendulum with jacobian

---
+++ build/CMakeFiles/double_pendulum.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/double_pendulum.dir/src/double_main.c.o"
+  "CMakeFiles/double_pendulum.dir/src/double_main.c.o.d"
+  "double_pendulum"
+  "double_pendulum.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/double_pendulum.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/single_pendulum.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/single_pendulum.dir/src/main.c.o"
+  "CMakeFiles/single_pendulum.dir/src/main.c.o.d"
+  "single_pendulum"
+  "single_pendulum.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/single_pendulum.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
